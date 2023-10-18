7618d730037568a6e0bebc7218b7809774533506
유영 <dbdud1999@naver.com>
2023-10-19 01:30:26 +0900

    :dizzy: 1504번 문제 풀이

--- src/BJ01000/BJ01504_특정한최단경로.java
+++ src/BJ01000/BJ01504_특정한최단경로.java
@@ -17,7 +17,7 @@ public class BJ01504_특정한최단경로 {
         long depth;
     }
     private static int N;
-    private static long MAX_VALUE;
+    private static final long MAX_VALUE = 200000000;
     private static PriorityQueue<Node> pq;
     private static long[][] edge;
 
@@ -33,17 +33,16 @@ public class BJ01504_특정한최단경로 {
         N = Integer.parseInt(st.nextToken());  // 노드의 수
         long E = Long.parseLong(st.nextToken());  // 간선의 수
         edge = new long[N + 1][N + 1];  // 간선 저장을 위한 2차원 배열
-        MAX_VALUE = 200000 * 1000 * 4 + 1;
-        for (int i = 1; i <= N; i++) {
-            Arrays.fill(edge[i], MAX_VALUE);
-        }
+//        for (int i = 1; i <= N; i++) {
+//            Arrays.fill(edge[i], MAX_VALUE);
+//        }
 
         int n1, n2;  // 임의의 노드 저장할 변수 선언
         for (long i = 0; i < E; i++) {  // 간선의 개수만큼 반복
             st = new StringTokenizer(br.readLine());
             n1 = Integer.parseInt(st.nextToken());  // 노드 1
             n2 = Integer.parseInt(st.nextToken());  // 노드 2
-            edge[n1][n2] = Math.min(Integer.parseInt(st.nextToken()), edge[n1][n2]);  // 노드 간의 거리 저장
+            edge[n1][n2] = Integer.parseInt(st.nextToken());  // 노드 간의 거리 저장
             edge[n2][n1] = edge[n1][n2];  // 양방향이므로 반대도 저장
         }
 
@@ -51,54 +50,42 @@ public class BJ01504_특정한최단경로 {
         int u = Integer.parseInt(st.nextToken());  // 지나야하는 노드 1 번호
         int v = Integer.parseInt(st.nextToken());  // 지나야하는 노드 2 번호
 
-        long fromStartToU = bfs(1, u, new boolean[N + 1], MAX_VALUE);
-        // System.out.println("fromStartToU: " + fromStartToU);
-        long fromStartToV = bfs(1, v, new boolean[N + 1], MAX_VALUE);
-        // System.out.println("fromStartToV: " + fromStartToV);
-        long fromUToV = bfs(u, v, new boolean[N + 1], MAX_VALUE);
-        // System.out.println("fromUToV: " + fromUToV);
-        long fromUToEnd = bfs(u, N, new boolean[N + 1], MAX_VALUE);
-        // System.out.println("fromUToEnd: " + fromUToEnd);
-        long fromVToEnd = bfs(v, N, new boolean[N + 1], MAX_VALUE);
-        // System.out.println("fromVToEnd: " + fromVToEnd);
-
         long min = MAX_VALUE;
-        if(fromStartToU < MAX_VALUE) {
-            if (fromVToEnd < MAX_VALUE) min = Math.min(min, fromStartToU + fromUToV + fromVToEnd);
-            if (fromUToEnd < MAX_VALUE) min = Math.min(min, fromStartToU + (fromUToV * 2) + fromUToEnd);
+        long fromStartToU = bfs(1, u, new long[N + 1], new boolean[N + 1]);
+        long fromStartToV = bfs(1, v, new long[N + 1], new boolean[N + 1]);
+        long fromUToV = bfs(u, v, new long[N + 1], new boolean[N + 1]);
+        long fromUToEnd = bfs(u, N, new long[N + 1], new boolean[N + 1]);
+        long fromVToEnd = bfs(v, N, new long[N + 1], new boolean[N + 1]);
+        if(fromStartToU < MAX_VALUE && fromUToV < MAX_VALUE && fromVToEnd < MAX_VALUE) {
+            min = Math.min(min, fromStartToU + fromUToV + fromVToEnd);
+        }
+        if(fromStartToV < MAX_VALUE && fromUToV < MAX_VALUE && fromUToEnd < MAX_VALUE) {
+            min = Math.min(min, fromStartToV + fromUToV + fromUToEnd);
         }
 
-        if(fromStartToV < MAX_VALUE) {
-            if (fromUToEnd < MAX_VALUE) min = Math.min(min, fromStartToV + fromUToV + fromUToEnd);
-            if (fromVToEnd < MAX_VALUE) min = Math.min(min, fromStartToV + (fromUToV * 2) + fromVToEnd);
-        }
         System.out.println((min >= MAX_VALUE)? -1 : min);
 
     }
 
-    private static long bfs(int startNode, int endNode, boolean[] visited, long min) {
-        if (startNode == endNode) return 0;  // 시작점과 끝점이 같은 경우 0 리턴
-
-        pq.clear();
+    private static long bfs(int startNode, int endNode, long[] dist, boolean[] visited) {pq.clear();
         pq.add(new Node(startNode, 0));
-        visited[startNode] = true;
+        Arrays.fill(dist, MAX_VALUE);
+        dist[startNode] = 0;
 
         long curDepth; Node curNode;
         while(!pq.isEmpty()) {
             curNode = pq.poll();
+            visited[curNode.index] = true;  // 방문 처리
             for (int i = 1; i <= N; i++) {
                 curDepth = curNode.depth + edge[curNode.index][i];  // 거리 더하기
-                if(visited[i]) continue;  // 방문했거나 못할 경우 다시 지나지 않기
-                if(i == endNode) {  // 끝점일 경우
-                    min = Math.min(min, curDepth);  // 값 비교
-                    continue;
-                }
-                visited[i] = true;  // 방문 처리
+                // System.out.println(curNode.index + "부터 " + edge[curNode.index][i] + "까지의 거리: " + curDepth);
+                if(edge[curNode.index][i] == 0 || visited[i] || dist[i] < curDepth) continue;  // 방문했거나 못할 경우 다시 지나지 않기
+                dist[i] = curDepth;
                 pq.add(new Node(i, curDepth));
             }
         }
 
-        // System.out.println(startNode + "부터 " + endNode + "까지의 거리: " + min);
-        return (min == 0)? MAX_VALUE: min;
+        // System.out.println(startNode + "부터 " + endNode + "까지의 거리: " + dist[endNode]);
+        return dist[endNode];
     }
 }
