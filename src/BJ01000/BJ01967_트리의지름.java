8552075523763cd92b62c59d42ce4ecc6bb760cc
유영 <dbdud1999@naver.com>
2023-10-23 20:54:00 +0900

    :dizzy: 1967번 문제 풀이

--- src/BJ01000/BJ01967_트리의지름.java
+++ src/BJ01000/BJ01967_트리의지름.java
@@ -7,66 +7,80 @@ import java.util.*;
 
 public class BJ01967_트리의지름 {
     private static class Node {
-        private Node(int S, int E) {
-            this.S = S; this.E = E;
+        private Node(int index, int weight) {
+            this.index = index; this.weight = weight;
         }
-
-        int S, E;  // 시작점과 끝점
+        int index, weight;
     }
     private static int N; // 노드의 개수
-    private static int[][] edge, dist;
+    private static List<Node>[] edge;  // 간선의 가중치 저장할 배열
+    private static int[] dist;  // 경로 가중치 저장할 배열
+    private static boolean[] visited;  // 방문 처리 배열
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());  // 노드의 개수 입력 받기
-        edge = new int[N + 1][N + 1];
 
-        int U, V;  // 노드1과 노드2를 담아줄 임의의 변수 선언
+        if(N == 1) {
+            System.out.println(0);
+            System.exit(0);
+        }
+
+        edge = new ArrayList[N + 1];
+        dist = new int[N + 1];  // 가중치 배열 초기화
+        visited = new boolean[N + 1];  // 방문 배열 초기화
+
+        int U, V, W;  // 노드1과 노드2, 가중치를 담아줄 임의의 변수 선언
         for (int i = 0; i < N - 1; i++) {  // 노드의 개수만큼 간선의 정보 입력 반복
             st = new StringTokenizer(br.readLine());
             U = Integer.parseInt(st.nextToken());
             V = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
 
-            edge[U][V] = Integer.parseInt(st.nextToken());  // 노드1 -> 노드2 가중치 초기화
-            edge[V][U] = edge[U][V];  // 노드2 -> 노드1 가중치 초기화
+            if(edge[U] == null) edge[U] = new ArrayList<>();
+            edge[U].add(new Node(V, W));  // 노드1 -> 노드2 가중치 초기화
+
+            if(edge[V] == null) edge[V] = new ArrayList<>();
+            edge[V].add(new Node(U, W));  // 노드2 -> 노드1 가중치 초기화
         }
 
-//        System.out.println("=== 간선의 가중치 ===");
-//        System.out.println(Arrays.deepToString(edge).replace("], ", "],\n"));
-
-        System.out.println(dijkstra());
+        int farIndex = findFarNode(1);  // 1에서부터 가장 먼 노드 찾기
+        farIndex = findFarNode(farIndex);  // 1에서 가장 먼 노드에서부터 가장 먼 노드 찾기
+        System.out.println(dist[farIndex]);
     }
 
-    private static int dijkstra() {
-        // 초기 변수 초기화
-        int[] dist = new int[N + 1];  // 최장 경로를 구할 dist 배열
-        boolean[] visited = new boolean[N + 1];  // 방문 처리 배열
-        Queue<Integer> queue = new ArrayDeque<>();
-        int max = 0;  // 최장 경로 저장할 변수
+    private static int findFarNode(int startNode) {  // 가장 가중치가 높은 노드 찾기
+        Queue<Integer> queue = new ArrayDeque<>();  // 최장거리를 계산할 큐
+        queue.add(startNode);  // 시작 노드 삽입
 
-        int curIndex;  // 큐에서 빼낼 Node 삽입할 임의 변수 선언
-        for (int i = 1; i <= N; i++) {
-            Arrays.fill(dist, 0);  // 거리 초기화
-            Arrays.fill(visited, false); // 방문 여부 초기화
-            queue.add(i);  // 시작 노드 큐에 삽입
-            visited[i] = true;  // 시작 노드 방문 처리
-            
-            while(!queue.isEmpty()) {  // 큐가 빌 때까지
-                curIndex = queue.poll();
-                for (int j = 1; j <= N; j++) {
-                    // 해당 노드를 방문한 적 없고, 간선이 존재하고, 시작 노드에서 현재 노드까지의 거리보다 중간 노드를 거쳐 가는 것이 더 클 경우
-                    if(!visited[j] && 0 < edge[curIndex][j] && dist[j] < dist[curIndex] + edge[curIndex][j]) {
-                        dist[j] = dist[curIndex] + edge[curIndex][j];  // 최대값 삽입
-                        visited[j] = true;
-                        queue.add(j);  // 이동된 i 삽입
-                    }
+        Arrays.fill(dist, 0);  // 거리 계산 배열 초기화
+        Arrays.fill(visited, false);  // 방문 배열 초기화
+        visited[startNode] = true;  // 시작 노드 방문 처리
+        int curIndex, selected = startNode;  // 큐에서 빼낸 값을 저장한 임의의 변수, 최장경로의 index 담을 변수 선언
+        while(!queue.isEmpty()) {
+            curIndex = queue.poll();
+            visited[curIndex] = true;
+            for (int i = 0; i < edge[curIndex].size(); i++) {
+                // 방문할 수 있고, 길이가 더 길 경우
+                if(!visited[edge[curIndex].get(i).index] && dist[edge[curIndex].get(i).index] < dist[curIndex] + edge[curIndex].get(i).weight) {
+                    dist[edge[curIndex].get(i).index] = dist[curIndex] + edge[curIndex].get(i).weight;  // 거리에 더 긴 값 삽입
+                    queue.add(edge[curIndex].get(i).index);  // 큐에 넣기
+                    if(dist[selected] < dist[edge[curIndex].get(i).index]) selected = edge[curIndex].get(i).index;  // 시작 노드에서 가장 거리가 먼 노드 삽입
                 }
             }
-            max = Math.max(max, dist[dist.length - 1]);
+            /* for (int i = 1; i <= N; i++) {
+                if(!visited[i] && edge[curIndex][i] != 0 && dist[i] < dist[curIndex] + edge[curIndex][i]) {
+                    dist[i] = dist[curIndex] + edge[curIndex][i];  // 거리에 더 긴 값 삽입
+                    queue.add(i);  // 큐에 넣기
+                    if(dist[selected] < dist[i]) selected = i;  // 시작 노드에서 가장 거리가 먼 노드 삽입
+                }
+            } */
         }
 
-        return max;
+        // System.out.println(startNode + "의 dist: " + Arrays.toString(dist));
+        // System.out.println(startNode + "에서의 최장 경로는 " + selected + " 인덱스까지의 "+ dist[selected]);
+        return selected;
     }
 }
