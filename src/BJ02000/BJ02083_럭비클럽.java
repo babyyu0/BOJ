09606b9a9a927da1911b1a6da8361e522306838c
유영 <dbdud1999@naver.com>
2023-11-12 00:03:18 +0900

    :dizzy: 2083번 문제 풀이

--- src/BJ02000/BJ02083_럭비클럽.java
+++ src/BJ02000/BJ02083_럭비클럽.java
@@ -1,4 +1,35 @@
 package BJ02000;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class BJ02083_럭비클럽 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        String name; int age, weight;
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+            name = st.nextToken();
+            age = Integer.parseInt(st.nextToken());
+            weight = Integer.parseInt(st.nextToken());
+
+            if(name.equals("#")) break;
+            sb.append(name).append(" ");
+            if(17 < age || 80 <= weight) {
+                sb.append("Senior");
+            } else {
+                sb.append("Junior");
+            }
+            sb.append("\n");
+        }
+
+        bw.append(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
