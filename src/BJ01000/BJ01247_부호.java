4831d8f38b2e4cc8eed7e75d70ffa173535b28ff
유영 <dbdud1999@naver.com>
2023-11-17 20:36:41 +0900

    :dizzy: 1247번 문제 풀이

--- src/BJ01000/BJ01247_부호.java
+++ src/BJ01000/BJ01247_부호.java
@@ -1,6 +1,34 @@
 package BJ01000;
 
+import java.io.*;
+import java.math.BigInteger;
+
 public class BJ01247_부호 {
 
-    private static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N; BigInteger sum;
+        for (int test_case = 0; test_case < 3; test_case++) {
+            N = Integer.parseInt(br.readLine());
+            sum = BigInteger.ZERO;
+
+            for (int i = 0; i < N; i++) {
+                sum = sum.add(new BigInteger(br.readLine()));
+                // System.out.println(sum);
+            }
+
+            if(sum == BigInteger.ZERO) sb.append("0");
+            else {
+                sb.append((sum.compareTo(BigInteger.ZERO) < 0)? "-": "+");
+            }
+            sb.append("\n");
+        }
+
+        bw.append(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
