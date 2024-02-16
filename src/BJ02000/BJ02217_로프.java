cf0402e20e8634e9182a8f63b8d2a93ac4c69523
유영 <dbdud1999@naver.com>
2024-02-17 02:18:37 +0900

    :dizzy: 2217번 문제 풀이

--- src/BJ02000/BJ02217_로프.java
+++ src/BJ02000/BJ02217_로프.java
@@ -1,7 +1,27 @@
 package BJ02000;
 
-public class BJ02217_로프 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 
+public class BJ02217_로프 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Integer[] ropes = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            ropes[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(ropes, Collections.reverseOrder());
+
+        int MAX_WEIGHT = ropes[0];
+        for (int i = 1; i < N; i++) {
+            MAX_WEIGHT = Math.max(MAX_WEIGHT, ropes[i] * (i + 1));
+        }
+
+        System.out.println(MAX_WEIGHT);
     }
 }
