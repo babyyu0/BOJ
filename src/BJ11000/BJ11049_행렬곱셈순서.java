1b0f297ffebff236ea824ac9c1252ebc0eb89da7
유영 <dbdud1999@naver.com>
2024-02-23 02:05:05 +0900

    :dizzy: 11049번 문제 풀이

--- src/BJ11000/BJ11049_행렬곱셈순서.java
+++ src/BJ11000/BJ11049_행렬곱셈순서.java
@@ -14,61 +14,49 @@ public class BJ11049_행렬곱셈순서 {
         }
 
         long r, c;
-
-        @Override
-        public String toString() {
-            return "Pos{" +
-                    "r=" + r +
-                    ", c=" + c +
-                    '}';
-        }
     }
 
     private static int N;
-    private static long MIN_MULTIPLY;  // 행렬의 개수
     private static Pos[] poses;  // 행렬을 담을 배열
-    private static long[][] cal, dp;  // 각 곱을 더할 배열
+    private static long[][] dp;  // 각 곱을 더할 배열
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        MIN_MULTIPLY = (long) Math.pow(2, 31);
         N = Integer.parseInt(br.readLine());
         poses = new Pos[N];
         dp = new long[N][N];
 
         for (int i = 0; i < N; i++) {
             Arrays.fill(dp[i], (long) Math.pow(2, 31));
-            dp[i][i] = 0;
-        }
-
-        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             poses[i] = new Pos(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));  // 행렬 담기
         }
 
-        System.out.println(Arrays.deepToString(dp).replace("],", "]\n") + "\n");
-        findMinMultiply();
-        System.out.println(Arrays.deepToString(dp).replace("],", "]\n") + "\n");
-
-        System.out.println(dp[0][N - 1]);
+        System.out.println(findMinMultiply());
     }
 
-    private static void findMinMultiply() {
-        for (int k = 0; k <= N; k++) {
-            for (int i = 1; i < N; i++) {  // i까지
-                for (int j = i - 1; j >= 0; j--) {  // j에서
-                    System.out.print("dp[" + (char)(j + 65) + "][" + (char)(i + 65) + "] = dp[" + (char)(j + 65) + "][" + (char)(i + 64) + "] + " + (poses[j].r * poses[i].r * poses[i].c));
-                    dp[j][i] = dp[j][i - 1] + poses[j].r * poses[i].r * poses[i].c;
-
-                    System.out.println(" || dp[" + (char)(j + 66) + "][" + (char)(i + 65) + "] + " + (poses[j].r * poses[j].c * poses[i].c));
-                    dp[j][i] = Math.min(poses[j].r * poses[j].c * poses[i].c + dp[j + 1][i], dp[j][i]);
-                    System.out.println("dp[" + (char)(j + 65) + "][" + (char)(i + 65) + "] = " + dp[j][i] );
+    private static long findMinMultiply() {
+        for (int i = 0; i < N; i++) { // i 까지
+            for (int j = i - 1; 0 <= j; j--) {  // j 에서
+                if (i - j == 1) {  // 무조건 결과 하나 (AB) 같은 경우
+                    dp[j][i] = (poses[j].r * poses[j].c * poses[i].c);
+                    continue;
                 }
+
+                dp[j][i] = Math.min(dp[j][i - 1] + (poses[j].r * poses[i].r * poses[i].c), dp[j][i]);  // (...) * n 값 찾기 ((AB)C) 같은 경우
+                dp[j][i] = Math.min((poses[j].r * poses[j].c * poses[i].c) + dp[j + 1][i], dp[j][i]);  // n * (...) 값 찾기 (A(BC)) 같은 경우
+
+                for (int k = j + 1; k < i - 1; k++) {  // 사이 계산 찾기 ((AB)(CD)) 같은 경우
+                    dp[j][i] = Math.min(dp[j][k] + dp[k + 1][i] + (poses[j].r * poses[k].c * poses[i].c), dp[j][i]);
+                }
+
             }
-            //System.out.println(Arrays.deepToString(dp).replace("],", "]\n") + "\n");
+
         }
+
+        return dp[0][N - 1];
     }
 }
