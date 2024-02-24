dd2cce3b755b906d86e62755eeb4254bca466770
유영 <dbdud1999@naver.com>
2024-02-24 23:53:15 +0900

    :dizzy: 1946번 문제 수정

--- src/BJ01000/BJ01946_신입사원.java
+++ src/BJ01000/BJ01946_신입사원.java
@@ -14,18 +14,8 @@ public class BJ01946_신입사원 {
         }
 
         int document, interview;
-
-        @Override
-        public String toString() {
-            return "Score{" +
-                    "document=" + document +
-                    ", interview=" + interview +
-                    '}';
-        }
     }
     private static final int MAX_CNT = 100000;
-    private static int N, MAX_PASS;
-    private static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,25 +24,22 @@ public class BJ01946_신입사원 {
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
         Score[] scores = new Score[MAX_CNT + 1];
-        visited = new boolean[MAX_CNT + 1];
+        int N, MAX_PASS;
 
         for (int tc = 0; tc < T; tc++) {
-            Arrays.fill(visited, false);
             Arrays.fill(scores, new Score());
             N = MAX_PASS = Integer.parseInt(br.readLine());
             for (int i = 0; i < N; i++) {
-                //System.out.println("i = " + i);
                 st = new StringTokenizer(br.readLine());
                 scores[i] = new Score(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
             }
             Arrays.sort(scores, (o1, o2) -> Integer.compare(o2.document, o1.document));
-            //System.out.println(Arrays.toString(Arrays.copyOf(scores, N)));
 
             for (int i = N - 2; 0 <= i; i--) {
                 if(scores[i].interview > scores[i + 1].interview) {
                     MAX_PASS--;
-                    scores[i].interview = Math.min(scores[i].interview, scores[i + 1].interview);
                 }
+                scores[i].interview = Math.min(scores[i].interview, scores[i + 1].interview);
             }
 
             sb.append(MAX_PASS).append("\n");
