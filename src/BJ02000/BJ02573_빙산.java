836ffb78811e466736743e2573c6dc3899eede5e
유영 <dbdud1999@naver.com>
2024-04-04 23:47:10 +0900

    :dizzy: 2573번 문제 풀이

---
+++ src/BJ02000/BJ02573_빙산.java
@@ -0,0 +1,21 @@
+package BJ02000;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class BJ02573_빙산 {
+    private static class Pos {
+        public Pos(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        int r, c;
+    }
+    private static int N, M;
+    private static List<Pos> posList;
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
